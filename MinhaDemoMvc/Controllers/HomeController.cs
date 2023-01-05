f6840753986edd387d2846eb44306b232d7deca2
Gustavo Luiz <gustavoluizmiranda405@gmail.com>
2023-01-05 14:30:45 -0300

    Initial commit

---
+++ MinhaDemoMvc/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMvc.Models;
+using System.Diagnostics;
+
+namespace MinhaDemoMvc.Controllers
+{
+    //[Route("")]
+    //[Route("gestao")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //[Route("")]
+        //[Route("pagina-inicial")]
+        //[Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(int id, Guid categoria)
+        {
+            var filme = new Filme
+            {
+                Titulo = "oi",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avaliacao = 10,
+                Valor = 20000
+            };
+            return RedirectToAction("Privacy", filme);
+            //return View();
+        }
+        //[Route("privacidade")]
+        //[Route("politica-de-privacidade")]
+        public IActionResult Privacy(Filme filme)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+        
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //[Route("erro")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MinhaDemoMvc/Models/Filme.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinhaDemoMvc.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo título é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O título precisa ter entre 3 e 60 caractéres")]
+        public string Titulo { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto")]
+        [Required(ErrorMessage = "O campo Data de Lançamento é obrigatório")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Genero em formato inválido")]
+        [StringLength(30, ErrorMessage = "Máximo de 30 caractéres"), Required(ErrorMessage = "O campo Gênero é obrigatório")]
+        public string Genero { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Required(ErrorMessage = "Preencha o campo valor")]
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [Display(Name = "Avaliação")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
+        public int Avaliacao { get; set; }
+    }
+}
